39600527a179ab0fe6a43ee60e78a81e5c468ca4
suniyahhh <123817061+suniyahhh@users.noreply.github.com>
2023-03-30 02:50:35 +0400

    TR commit

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
@@ -31,6 +31,20 @@ public class TrainingRecord {
        return result;
    } // lookupEntry
    
+   
+   // look up all entries of a given day and month
+   public String lookupAll (int d, int m, int y) {
+       ListIterator<Entry> iter = tr.listIterator();
+       String result = "";
+       while (iter.hasNext()) {
+          Entry current = iter.next();
+          if (current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
+             result += current.getEntry();
+            }
+       if (result.equals("")) {result = "No entries found";};
+       return result;
+   } // lookupAll
+   
    // Count the number of entries
    public int getNumberOfEntries(){
        return tr.size();
@@ -39,5 +53,37 @@ public class TrainingRecord {
    public void clearAllEntries(){
        tr.clear();
    }
+   
+   // check if entry with same name+date and time has been added before
+   public boolean checkDuplicate (String n, int d, int m, int y) {
+       ListIterator<Entry> iter = tr.listIterator();
+       boolean result = false;
+       while (iter.hasNext()) {
+          Entry current = iter.next();
+          if (current.getName().equals(n) && current.getDay()==d && current.getMonth()==m && current.getYear()==y) {
+        	  result = true;
+        	  return result;
+          }  
+       }
+       return result;
+   } // checkDuplicate
+   
+   
+   public String removeRec(String n, int d, int m, int y) {
+	   ListIterator<Entry> iter = tr.listIterator();
+       String result = "No entries found";
+       Entry found = null;
+       while (iter.hasNext()) {
+          Entry current = iter.next();
+          if (current.getName().equals(n) && current.getDay()==d && current.getMonth()==m && current.getYear()==y) {
+        	  found = current;
+        	  result = "Record successfully removed.";
+          }
+       }
+       tr.remove(found);
+       return result;
+
+   }
+
    
 } // TrainingRecord
